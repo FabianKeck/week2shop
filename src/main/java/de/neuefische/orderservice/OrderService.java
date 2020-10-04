39d2070eab1e30623b8cb2fa019de084a442a4fd
Fabian Keck <fabian.keck@posteo.de>
2020-10-04 11:23:19 +0200

    Add class OrderService

--- src/main/java/de/neuefische/orderservice/OrderService.java
+++ src/main/java/de/neuefische/orderservice/OrderService.java
@@ -1,4 +1,18 @@
 package de.neuefische.orderservice;
 
+import de.neuefische.orderdb.OrderDB;
+import de.neuefische.orderdb.OrderDBMemory;
+import de.neuefische.productDB.Product;
+import de.neuefische.productDB.ProductDB;
+import de.neuefische.productDB.ProductDBMemory;
+
+import java.util.ArrayList;
+
 public class OrderService {
+    private ProductDB productDB;
+    private OrderDB orderDB = new OrderDBMemory();
+
+    public OrderService(ArrayList<Product> products){
+        productDB= new ProductDBMemory(products);
+    }
 }
